605c50978879f2b9ba70b9697b0c7e8d132711cc
Donald Mellenbruch <hello@Fresh2.dev>
2023-08-10 00:00:00 -0500

    feat: if first arg is file, invoke tui from file

--- src/tuiview/__main__.py
+++ src/tuiview/__main__.py
@@ -1,22 +1,34 @@
+import os
 import sys
+from argparse import ArgumentParser
 from importlib import import_module
 from pathlib import Path
-from typing import Any, Callable, Dict
+from typing import Any, Callable, Dict, List
 
 import yapx
 from argparse_tui import invoke_tui
-from yapx.types import Annotated
 
 from .__version__ import __version__
 
 
-def from_file(*args, path: Annotated[Path, yapx.arg(pos=True)]):
+def invoke_tui_from_file(path: Path, *args):
+    if not path.exists():
+        raise FileNotFoundError(path)
     sys.path.append(str(path.parent))
     module = import_module(path.stem)
+    candidate_attrs: List[str] = ["parser", "tui", "form", "cli", "app"]
+    parser_attr: str = candidate_attrs[0]
+    for attr in candidate_attrs:
+        if hasattr(module, attr):
+            parser_attr = attr
+            break
+
+    parser: ArgumentParser = getattr(module, parser_attr)
+    if not isinstance(parser, ArgumentParser):
+        err: str = f"The value of '{parser_attr}' is not an argparse.ArgumentParser"
+        raise TypeError(err)
 
     invoke_tui(module.parser, cli_args=args)
-
-    sys.exit(0)
 
 
 def main() -> None:
@@ -30,14 +42,17 @@ def main() -> None:
             )
             named_subcommands[x.stem.replace("_", "-")] = cmd_module.main
 
-    yapx.run_commands(
-        [from_file],
-        named_subcommands=named_subcommands,
-        prog_version=__version__,
-        default_args=["--help"],
-        tui_flags=[],
-    )
-
-
-if __name__ == "__main__":
-    main()
+    if sys.argv and ("." in sys.argv[1] or os.sep in sys.argv[1]):
+        try:
+            invoke_tui_from_file(Path(sys.argv[1]), *sys.argv[2:])
+        except FileNotFoundError as e:
+            print("File not found:", e)
+        except (AttributeError, TypeError) as e:
+            print(e)
+    else:
+        yapx.run_commands(
+            named_subcommands=named_subcommands,
+            prog_version=__version__,
+            default_args=["--help"],
+            tui_flags=[],
+        )
